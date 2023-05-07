108484a3d811627044dc5a2ba04572cda2a84f63
wacdev <i@wac.tax>
2023-05-07 17:36:02 +0800

    ♨

--- coffee/main.coffee
+++ coffee/main.coffee
@@ -5,33 +5,31 @@
   path > join dirname
   @w5/uridir
 
-Eq = (func, args...)=>
-  (result)=>
-    {name} = func
-    test(
-      name
-      (t)=>
-        r = func(...args)
-        if r instanceof Promise
-          name = 'await '+name
-        console.log '    '+name+'(', args.map(
-          (i)=>JSON.stringify i
-        ).join(','), ')', '=',result
-        if r instanceof Promise
-          r = await r
-        t.deepEqual(
-          r
-          result
-        )
-        return
+Eq = (func)=>
+  (args...)=>
+    (result)=>
+      {name} = func
+      test(
+        name
+        (t)=>
+          r = func(...args)
+          if r instanceof Promise
+            name = 'await '+name
+          console.log '    '+name+'(', args.map(
+            (i)=>JSON.stringify i
+          ).join(','), ')', '=',result
+          if r instanceof Promise
+            r = await r
+          t.deepEqual(
+            r
+            result
+          )
+          return
 
-    )
+      )
 
 
-Eq(
-  zipU64
-  [1,2,3,4]
-)(
+Eq(zipU64)([1,2,3,4])(
   Buffer.from [1,2,3,4]
 )
 
--- test/main.mjs
+++ test/main.mjs
@@ -14,25 +14,27 @@ import {
 
 import uridir from '@w5/uridir';
 
-Eq = (func, ...args) => {
-  return (result) => {
-    var name;
-    ({name} = func);
-    return test(name, async(t) => {
-      var r;
-      r = func(...args);
-      if (r instanceof Promise) {
-        name = 'await ' + name;
-      }
-      console.log('    ' + name + '(', args.map((i) => {
-        return JSON.stringify(i);
-      }).join(','), ')', '=', result);
-      if (r instanceof Promise) {
-        r = (await r);
-      }
-      t.deepEqual(r, result);
-    });
+Eq = (func) => {
+  return (...args) => {
+    return (result) => {
+      var name;
+      ({name} = func);
+      return test(name, async(t) => {
+        var r;
+        r = func(...args);
+        if (r instanceof Promise) {
+          name = 'await ' + name;
+        }
+        console.log('    ' + name + '(', args.map((i) => {
+          return JSON.stringify(i);
+        }).join(','), ')', '=', result);
+        if (r instanceof Promise) {
+          r = (await r);
+        }
+        t.deepEqual(r, result);
+      });
+    };
   };
 };
 
-Eq(zipU64, [1, 2, 3, 4])(Buffer.from([1, 2, 3, 4]));
+Eq(zipU64)([1, 2, 3, 4])(Buffer.from([1, 2, 3, 4]));
