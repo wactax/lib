d38feb109ae227cb0e537389a54fa570040351f3
wacdev <i@wac.tax>
2023-05-07 21:46:31 +0800

    ♨

--- coffee/main.coffee
+++ coffee/main.coffee
@@ -1,35 +1,36 @@
 #!/usr/bin/env coffee
 
-> ../index.js > zipU64
+> ../index.js
   ava:test
   path > join dirname
   @w5/uridir
 
-Eq = (func)=>
-  (args...)=>
-    (result)=>
-      {name} = func
-      test(
-        name
-        (t)=>
-          r = func(...args)
-          if r instanceof Promise
-            name = 'await '+name
-          console.log '    '+name+'(', args.map(
-            (i)=>JSON.stringify i
-          ).join(','), ')', '=',result
-          if r instanceof Promise
-            r = await r
-          t.deepEqual(
-            r
-            result
-          )
-          return
+T = new Proxy(
+  {}
+  get:(_,name)=>
+    func = index[name]
+    (args...)=>
+      (result)=>
+        test(
+          name
+          (t)=>
+            r = func(...args)
+            if r instanceof Promise
+              name = 'await '+name
+            console.log '    '+name+'(', args.map(
+              (i)=>JSON.stringify i
+            ).join(','), ')', '=',result
+            if r instanceof Promise
+              r = await r
+            t.deepEqual(
+              r
+              result
+            )
+            return
+        )
+)
 
-      )
-
-
-Eq(zipU64)([1,2,3,4])(
+T.zipU64(1,2,3,4)(
   Buffer.from [1,2,3,4]
 )
 
--- test/main.mjs
+++ test/main.mjs
@@ -1,9 +1,7 @@
 #!/usr/bin/env -S node --loader=@w5/jsext --trace-uncaught --expose-gc --unhandled-rejections=strict --experimental-import-meta-resolve
-var Eq;
+var T;
 
-import {
-  zipU64
-} from '../index.js';
+import index from '../index.js';
 
 import test from 'ava';
 
@@ -14,27 +12,29 @@ import {
 
 import uridir from '@w5/uridir';
 
-Eq = (func) => {
-  return (...args) => {
-    return (result) => {
-      var name;
-      ({name} = func);
-      return test(name, async(t) => {
-        var r;
-        r = func(...args);
-        if (r instanceof Promise) {
-          name = 'await ' + name;
-        }
-        console.log('    ' + name + '(', args.map((i) => {
-          return JSON.stringify(i);
-        }).join(','), ')', '=', result);
-        if (r instanceof Promise) {
-          r = (await r);
-        }
-        t.deepEqual(r, result);
-      });
+T = new Proxy({}, {
+  get: (_, name) => {
+    var func;
+    func = index[name];
+    return (...args) => {
+      return (result) => {
+        return test(name, async(t) => {
+          var r;
+          r = func(...args);
+          if (r instanceof Promise) {
+            name = 'await ' + name;
+          }
+          console.log('    ' + name + '(', args.map((i) => {
+            return JSON.stringify(i);
+          }).join(','), ')', '=', result);
+          if (r instanceof Promise) {
+            r = (await r);
+          }
+          t.deepEqual(r, result);
+        });
+      };
     };
-  };
-};
+  }
+});
 
-Eq(zipU64)([1, 2, 3, 4])(Buffer.from([1, 2, 3, 4]));
+T.zipU64(1, 2, 3, 4)(Buffer.from([1, 2, 3, 4]));
