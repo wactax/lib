4e11a49fbc714f09beebc07210c023bf005191b4
wacdev <i@wac.tax>
2023-05-07 22:40:21 +0800

    ♨

--- coffee/main.coffee
+++ coffee/main.coffee
@@ -1,35 +1,12 @@
 #!/usr/bin/env coffee
 
 > ../index.js
-  ava:test
+  @w5/avat
   path > join dirname
   @w5/uridir
   util
 
-T = new Proxy(
-  {}
-  get:(_,name)=>
-    func = index[name]
-    (args...)=>
-      (result)=>
-        test(
-          name
-          (t)=>
-            r = func(...args)
-            if r instanceof Promise
-              name = 'await '+name
-            console.log '`'+name+'(', args.map(
-              (i)=>JSON.stringify i
-            ).join(','), ')`', ' → `'+util.format(result)+'`\n'
-            if r instanceof Promise
-              r = await r
-            t.deepEqual(
-              r
-              result
-            )
-            return
-        )
-)
+T = avat index
 
 T.zipU64(1,2,3,4)(
   Buffer.from [1,2,3,4]
--- test/main.mjs
+++ test/main.mjs
@@ -3,7 +3,7 @@ var T;
 
 import index from '../index.js';
 
-import test from 'ava';
+import avat from '@w5/avat';
 
 import {
   join,
@@ -14,29 +14,6 @@ import uridir from '@w5/uridir';
 
 import util from 'util';
 
-T = new Proxy({}, {
-  get: (_, name) => {
-    var func;
-    func = index[name];
-    return (...args) => {
-      return (result) => {
-        return test(name, async(t) => {
-          var r;
-          r = func(...args);
-          if (r instanceof Promise) {
-            name = 'await ' + name;
-          }
-          console.log('`' + name + '(', args.map((i) => {
-            return JSON.stringify(i);
-          }).join(','), ')`', ' → `' + util.format(result) + '`\n');
-          if (r instanceof Promise) {
-            r = (await r);
-          }
-          t.deepEqual(r, result);
-        });
-      };
-    };
-  }
-});
+T = avat(index);
 
 T.zipU64(1, 2, 3, 4)(Buffer.from([1, 2, 3, 4]));
