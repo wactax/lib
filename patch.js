b7a9ab5b28f202b8f778002670a61c6c197974db
wacdev <i@wac.tax>
2023-05-08 18:47:57 +0800

    ♨

--- patch.js
+++ patch.js
@@ -1,27 +1,32 @@
 import autoe from "@w5/utf8/autoe.js";
 import { u8merge } from "@w5/u8";
 
-const { tld: _tld, z85Dump: _z85Dump, zipU64: _zipU64 } = nativeBinding;
+const { zipU64: _zipU64 } = nativeBinding;
 
-const autoeLi = new Proxy(
-	{},
-	{
-		get: (_, name) => {
-			const func = nativeBinding[name];
-			nativeBinding[name] = (...args) => {
-				return func(u8merge(...args.map(autoe)));
-			};
-			return;
-		},
-	},
-);
-
-autoeLi.passwordHash;
-autoeLi.cookieEncode;
-autoeLi.xxh64;
-autoeLi.xxh32;
-autoeLi.xxh3B36;
-
-nativeBinding.tld = (domain) => _tld(autoe(domain));
-nativeBinding.z85Dump = (s) => _z85Dump(autoe(s));
 nativeBinding.zipU64 = (...args) => _zipU64(args);
+
+const wrap = (w) =>
+	new Proxy(
+		{},
+		{
+			get: (_, name) => {
+				const func = nativeBinding[name];
+				nativeBinding[name] = w(func);
+				return;
+			},
+		},
+	);
+
+const Autoe = wrap((func) => (s) => func(autoe(s)));
+
+const AutoeLi = wrap((func) => (...args) => func(u8merge(...args.map(autoe))));
+
+AutoeLi.passwordHash;
+AutoeLi.cookieEncode;
+AutoeLi.xxh64;
+AutoeLi.xxh32;
+AutoeLi.xxh3B36;
+
+Autoe.tld;
+Autoe.z85Dump;
+Autoe.z85Load;
